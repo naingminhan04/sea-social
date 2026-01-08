541fdabeeae9f36dbefec20abc70b09452f06474
elliot3008 <naingminhan@icloud.com>
2026-01-09 00:19:19 +0630

    fix navigation blocking

---
+++ app/(main)/post/[postId]/loading.tsx
@@ -0,0 +1,5 @@
+export default function Loading() {
+    return <div className="flex justify-center items-center w-full h-[calc(100dvh-68px)] lg:h-dvh">
+        <span className="w-10 h-10 rounded-full border-2 border-white/30 border-t-white animate-spin" />
+      </div>
+}
