e60e234abb69fa950b6359cf6baa9dd9d996759f
elliot3008 <naingminhan@icloud.com>
2026-01-26 00:45:39 +0630

    fix small profile view ui

--- app/(main)/user/[username]/page.tsx
+++ app/(main)/user/[username]/page.tsx
@@ -1,12 +1,12 @@
 import Profile from "@/app/_components/Profile";
 
-const OtherUserProfile = async ({ params }: {params: {username: string}}) => {
+const UserProfile = async ({ params }: {params: {username: string}}) => {
   const {username}  = await params;
   return (
-    <div className="px-2">
+    <div className="md:px-2">
       <Profile userId={`${username}`}/>
     </div>
   );
 };
 
-export default OtherUserProfile;
+export default UserProfile;
